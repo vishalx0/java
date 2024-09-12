4e0ce9aa0e6a94bb788a0e61629f9ec3de97a9fe
vishalx0 <vishal00ypr@gmail.com>
2024-09-12 11:59:49 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,18 @@
+
+public class hello {
+    public static void add(int n){
+        if(n>10){
+            return;
+        }
+        System.out.println(n);
+        add(n+1);
+    }
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number : ");
+        // int n = sc.nextInt();
+        add(1);
+        
+    }
+    }
+
