5da9edb751b32fb19957ba77443d44cec4a42c36
vishalx0 <vishal00ypr@gmail.com>
2024-09-12 12:04:48 +0530

    modify

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class hello {
     public static void add(int n){
         if(n>10){
@@ -8,10 +8,10 @@ public class hello {
         add(n+1);
     }
     public static void main(String[] args){
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter a number : ");
-        // int n = sc.nextInt();
-        add(1);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int n = sc.nextInt();
+        add(n);
         
     }
     }
